62365e22333c9f351216b81b6c174511ee6eb880
rifanid98 <adninsijawa.office@gmail.com>
2020-08-09 16:40:10 +0700

    products route

---
+++ src/routes/r_products.js
@@ -0,0 +1,48 @@
+/**
+ * ExpressJs FW
+ */
+// import express framework
+const express = require('express');
+
+// instance of express router
+const router = express.Router();
+
+// import multer upload helper
+const upload = require('../helpers/upload');
+
+// import middlewares
+const authMiddleware = require('../middlewares/mdl_auth');
+
+/**
+ * Controllers
+ */
+// load the controller
+const productsController = require('../controllers/c_products');
+
+
+/**
+ * Main CRUD
+ */
+// Get All Products
+router.get('/', productsController.getProducts);
+// Post a Product
+router.post('/', authMiddleware.checkRole([2, 1]), upload.single('image'), productsController.postProduct);
+// Patch a Product
+router.patch('/:id', authMiddleware.checkRole([2, 1]), upload.single('image'), productsController.patchProduct);
+// Delete a Product
+router.delete('/:id', authMiddleware.checkRole([1]), productsController.deleteProduct);
+
+/**
+ * Other CRUD
+ */
+// Get Product By ID
+router.get('/:id', productsController.getProductById);
+/**
+ * Use authMiddleware example.
+ * 1. Admin
+ * 2. Staff
+ * 3. Product
+ */
+// router.get('/', authMiddleware.checkRole([3, 2, 1]), booksController.getProducts);
+
+module.exports = router;
